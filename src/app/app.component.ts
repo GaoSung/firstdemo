a73dc7bcd116ace84ccfab0e1b790bcdc31f2229
gao-xng <gaoxianglvp@163.com>
2016-12-06 23:50:25 +0800

    angular2 demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  private url = 'http://apis.baidu.com/apistore/idservice/id?id=420984198704207896&apikey=1b4a468e948d9ae30727d19005c71c2f';
+
+  constructor(http: Http) {
+    http.get(this.url)
+      .map(res => res.json())
+      .subscribe(people => this.title = JSON.stringify(people));
+  }
 }
